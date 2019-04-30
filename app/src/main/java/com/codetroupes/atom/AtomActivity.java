f0b90f4342a86e36a901e77c765ff8f56f92146e
Jeffrey <toyk1ng@qq.com>
2019-04-30 11:34:54 +0800

    Initial commit

---
+++ app/src/main/java/com/codetroupes/atom/AtomActivity.java
@@ -0,0 +1,21 @@
+package com.codetroupes.atom;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class AtomActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_atom);
+
+        TextView textView = findViewById(R.id.text);
+
+
+
+
+
+    }
+}
