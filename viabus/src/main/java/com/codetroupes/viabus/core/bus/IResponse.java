4a51abe94e8573e8674e4d06ded0d820d064754f
Jeffrey <toyk1ng@qq.com>
2019-05-17 09:14:14 +0800

    加入viabus https://github.com/KunMinX/android-viabus-architecture

---
+++ viabus/src/main/java/com/codetroupes/viabus/core/bus/IResponse.java
@@ -0,0 +1,11 @@
+package com.codetroupes.viabus.core.bus;
+
+/**
+ * @author KunMinX
+ * Create at 2018/8/22
+ */
+public interface IResponse {
+
+    void onResult(Result testResult);
+
+}
---
+++ viabus/src/main/java/com/codetroupes/viabus/core/bus/ResultCode.java
@@ -0,0 +1,10 @@
+package com.codetroupes.viabus.core.bus;
+
+/**
+ * @author KunMinX
+ * Create at 2018/8/22
+ */
+public class ResultCode {
+    public static final String FAILURE = "FAILURE";
+    public static final String CANCELED = "CANCELED";
+}
